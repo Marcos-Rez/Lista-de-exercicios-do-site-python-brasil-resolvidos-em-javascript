af0e1d605c73d21208ff5800cece35ead4592952
Marcos-Rez <rezendemarcos102@gmail.com>
2023-02-01 13:49:46 -0300

    Ex 50

--- Estrutura de Repetição/Ex.46-50/Ex.48-50.js
+++ Estrutura de Repetição/Ex.46-50/Ex.48-50.js
@@ -5,20 +5,12 @@
 */
 
 /*
-Faça um programa que mostre os n termos da Série a seguir:
-
-      S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m. 
-
-    Imprima no final a soma da série.
-*/
-
-/*
  48 - Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
     Exemplo:
       12376489
       => 98467321
  */
-/*
+
 var arr = [];
 var numeroInteiro = parseInt(prompt("Informe um numero inteiro positivo"));
 while(numeroInteiro < 0 || isNaN(numeroInteiro)){
@@ -31,7 +23,7 @@ for(let i = numeroInteiro.length; i >= 0; i--){
 }
 let saida = arr.join("");
 alert(saida)
-*/
+
 
 /*
   49 - Faça um programa que mostre os n termos da Série a seguir:
@@ -39,7 +31,7 @@ alert(saida)
       Imprima no final a soma da série. 
 */
 var h = 0;
-var n = 4;
+var n = 5;
 
 var arr = [];
 arr.push(`S = `);
@@ -62,20 +54,25 @@ for(let i = 0; i < n; i++){
 }
 
 var s1 = arr.join("");
-
+var soma = 1;
 var Ex49Soma = 0;
+alert(s1);
 
-for(let i = 0; i < arr.length; i++){
-  if(i % 2 != 0 && i != 0 && i < 8){
-    console.log(arr[i]);
-    Ex49Soma += arr[i];
+for(let i = 0; i < n; i++){
+  if(i == 0){
+     Ex49Soma += (i+1) / (i+1);
+  }if(i == 1){
+    Ex49Soma += (i+1) / (i+2);
+  }if(i > 1){
+    Ex49Soma += (i+1) / (i+soma);
   }
+  soma++;
 }
-console.log(s1);
+
+alert(" A soma dos n termos é: "+Ex49Soma);
 
 
 
-/*
 //50 - Sendo H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, Faça um programa que calcule o valor de H com N termos.
 var arr2 = [];
 for(let i = 2; i <= n; i++){
@@ -84,10 +81,6 @@ for(let i = 2; i <= n; i++){
 
 }
 var t = arr2.length;
-console.log(` Sendo H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/${n}, o valor de H é: ${arr2[t-1]}`)
+alert(` Sendo H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/${n}, o valor de H é: ${arr2[t-1]}`)
 
-/*
- 51 - Faça um programa que mostre os n termos da Série a seguir:
-      S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m. 
-      Imprima no final a soma da série.  
-*/
+
